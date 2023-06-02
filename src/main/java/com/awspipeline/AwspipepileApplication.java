742a5ff3da89871ed4b907a78c7e019e31f36847
NaajaFouzy245210 <245210@ust.com>
2023-06-02 16:28:56 +0530

    first commit

---
+++ src/main/java/com/awspipeline/AwspipepileApplication.java
@@ -0,0 +1,13 @@
+package com.awspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwspipepileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwspipepileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awspipeline/AwspipepileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwspipepileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
